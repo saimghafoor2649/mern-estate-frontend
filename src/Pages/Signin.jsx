8c5324b4c6808fb688b0369ccd0e3eae6ebfdf0d
saimghafoor2649 <saimghafoor2649@gmail.com>
2025-02-23 20:31:59 +0500

    update signin

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -2,8 +2,33 @@ import React from "react";
 
 export default function Signin() {
   return (
-    <div>
-      <h1>Signin</h1>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          className="border p-3 rounded-lg bg-white"
+          type="text"
+          placeholder="username"
+          id="username"
+        />
+        <input
+          className="border p-3 rounded-lg bg-white"
+          type="email"
+          placeholder="email"
+          id="email"
+        />
+        <input
+          className="border p-3 rounded-lg bg-white"
+          type="password"
+          placeholder="password"
+          id="password"
+        />
+        <Link to="/">
+          <button className="bg-slate-700 text-white p-3 rounded-lg text-uppercase hover:opacity-90 disabled:opacity-80">
+            Signin
+          </button>
+        </Link>
+      </form>
     </div>
   );
 }
